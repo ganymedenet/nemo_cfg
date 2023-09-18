2797208d372480e3af59e10aaec05fdb6b66535b
ganymedenet <51125243+ganymedenet@users.noreply.github.com>
2023-09-18 21:44:27 +0100

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,6 +1,5 @@
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo dpkg -i cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo cp /var/cuda-repo-debian11-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo add-apt-repository contrib
-sudo apt-get update
-sudo apt-get -y install cuda
+# https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
+
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+
+sudo python3 install_gpu_driver.py
