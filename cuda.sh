23571bf455cc0a23f9f2d8cc406e4e7f21e551d8
ganymedenet <51125243+ganymedenet@users.noreply.github.com>
2023-09-18 21:42:46 +0100

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,15 +1,6 @@
-sudo apt install -y linux-headers-$(uname -r)
-
-sudo apt install -y gnupg2 curl
-curl -O https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/cuda-repo-debian10_11.0.3-1_amd64.deb
-sudo dpkg -i cuda-repo-debian10_11.0.3-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/7fa2af80.pub
-sudo apt update
-
-sudo apt install -y cuda
-
-echo 'export PATH=/usr/local/cuda-11.0/bin${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
-source ~/.bashrc
-
-nvcc --version
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-debian11-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo add-apt-repository contrib
+sudo apt-get update
+sudo apt-get -y install cuda
