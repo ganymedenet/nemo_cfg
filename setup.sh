caf40176070aaae15eef041f6bd2004c3abb73af
ganymedenet <51125243+ganymedenet@users.noreply.github.com>
2023-09-18 19:06:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ mkdir -p datasets
 # Fetch the get_librispeech_data.py script from NeMo repository
 wget https://raw.githubusercontent.com/NVIDIA/NeMo/main/scripts/dataset_processing/get_librispeech_data.py
 
-pip3 install sox
+pip3 install sox tqdm
 
 # Run the script to download the LibriSpeech dataset parts
 python3 get_librispeech_data.py --data_root=datasets --data_set="dev_clean,test_clean"
