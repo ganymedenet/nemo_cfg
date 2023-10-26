6cae22fa0cd4ef7cea6740140a67f6b1bcd14ce6
ganynmednet <ganymednet@gmail.com>
2023-10-26 08:46:28 +0100

    confing

--- qai/docker.sh
+++ qai/docker.sh
@@ -4,7 +4,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
--- qai/nginx.sh
+++ qai/nginx.sh
@@ -1,5 +1,5 @@
 
-sudo apt-get install openssl
+sudo apt-get install -y openssl
 sudo apt-get install -y nginx
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt
 sudo openssl dhparam -out /etc/nginx/dhparam.pem 4096
