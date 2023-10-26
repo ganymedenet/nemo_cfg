d8efd8496fd35bac361264908f0e8f27e3fea1f6
ganynmednet <ganymednet@gmail.com>
2023-10-26 09:02:10 +0100

    confing

--- qai/readme.md
+++ qai/readme.md
@@ -1,5 +1,5 @@
 
-curl -o setup.sh -fsSL https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/setup.sh
+curl -o setup.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/setup.sh
 chmod +x setup.sh
 yes | ./setup.sh
 
--- qai/setup.sh
+++ qai/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-curl -o docker.sh -fsSL https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/docker.sh
+curl -o docker.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/docker.sh
 chmod +x docker.sh
 yes | ./docker.sh
 
 
-curl -o gpu.sh -fsSL https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/gpu.sh
+curl -o gpu.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/gpu.sh
 chmod +x gpu.sh
 yes | ./gpu.sh
 
-curl -o nginx.sh -fsSL https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/nginx.sh
+curl -o nginx.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/nginx.sh
 chmod +x nginx.sh
 yes | ./nginx.sh
