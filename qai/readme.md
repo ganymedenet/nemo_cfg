a362f204d80139f0e2f55da0aa71f7aca7ae5c38
ganynmednet <ganymednet@gmail.com>
2023-10-26 09:04:34 +0100

    confing

--- qai/readme.md
+++ qai/readme.md
@@ -1,6 +1,6 @@
 
 curl -o setup.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/setup.sh
 chmod +x setup.sh
-yes | ./setup.sh
+./setup.sh
 
 34.28.17.91
--- qai/setup.sh
+++ qai/setup.sh
@@ -2,13 +2,13 @@
 
 curl -o docker.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/docker.sh
 chmod +x docker.sh
-yes | ./docker.sh
+./docker.sh
 
 
 curl -o gpu.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/gpu.sh
 chmod +x gpu.sh
-yes | ./gpu.sh
+./gpu.sh
 
 curl -o nginx.sh -L https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/nginx.sh
 chmod +x nginx.sh
-yes | ./nginx.sh
+./nginx.sh
