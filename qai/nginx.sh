686186ebd832702fb3470ae8fb50f12217d6ed03
ganynmednet <ganymednet@gmail.com>
2023-10-26 20:08:19 +0100

    confing

--- qai/nginx.sh
+++ qai/nginx.sh
@@ -13,3 +13,16 @@ fi
 sudo curl -o /etc/nginx/sites-available/default https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/nginx/sites-available
 sudo nginx -t
 sudo systemctl reload nginx
+
+
+
+#crt_file="/etc/ssl/certs/nginx.crt"
+#key_file="/etc/ssl/private/nginx.key"
+#
+#if [ ! -e "$crt_file" ] || [ ! -e "$key_file" ]; then
+#  echo "Hie"
+#fi
+#
+#if [ ! -e "/etc/ssl/certs/nginx.crt" ] || [ ! -e "/etc/ssl/private/nginx.key"]; then
+#  echo "hie"
+#fi
