bf184ddb0dd86103eeef1ed1e1925a6573a7009c
ganynmednet <ganymednet@gmail.com>
2023-10-31 22:22:25 +0000

    training conf

--- training/bash/config.sh
+++ training/bash/config.sh
@@ -1,20 +1,14 @@
 
 
 # Download vits.py
-wget https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/training/scripts/vits.py
+wget https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/training/scripts/vits.py
 
 # Create the 'conf' directory and download vits.yaml into it
-mkdir conf
-wget -O https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/qai/training/scripts/vits.yaml
-
+mkdir -p conf && cd conf
+wget https://raw.githubusercontent.com/ganymedenet/nemo_cfg/master/training/scripts/vits.yaml && cd ..
 
 BRANCH="main"
-#mkdir -p tts_dataset_files && cd tts_dataset_files
-#wget https://raw.githubusercontent.com/NVIDIA/NeMo/$BRANCH/scripts/tts_dataset_files/ipa_cmudict-0.7b_nv23.01.txt
-#wget https://raw.githubusercontent.com/NVIDIA/NeMo/$BRANCH/scripts/tts_dataset_files/heteronyms-052722
-#cd ..
 
-# Download ipa_cmudict-0.7b_nv23.01.txt to the 'conf' directory
 wget -O conf/ipa_cmudict-0.7b_nv23.01.txt https://raw.githubusercontent.com/NVIDIA/NeMo/$BRANCH/scripts/tts_dataset_files/ipa_cmudict-0.7b_nv23.01.txt
 
 # Download heteronyms-052722 to the 'conf' directory
--- training/bash/install.sh
+++ training/bash/install.sh
@@ -1,5 +1,5 @@
 # Install sox using apt-get
-sudo apt-get install sox
+sudo apt-get -y install sox
 
 # Install the sox Python package using pip
 pip install sox
