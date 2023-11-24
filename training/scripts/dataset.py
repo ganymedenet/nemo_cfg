36feda97f036d1bb0957958ef97c5610ba035ed8
ganynmednet <ganymednet@gmail.com>
2023-11-24 20:02:25 +0000

    training conf

--- training/scripts/dataset.py
+++ training/scripts/dataset.py
@@ -17,7 +17,7 @@ for line in lines:
     # print(ln)
     ln["audio_filepath"] = f"{current_directory}/datasets/{ln['audio_filepath']}"
 
-    if 5 < ln["duration"] < 20:
+    if 1 < ln["duration"] < 10:
         final.append(f"{json.dumps(ln)}\n")
 
 # Calculate the split index (80% of the total)
